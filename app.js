2d8868d98519ac815902e03816bb47814f2e94a2
Tatiana <tlekhatkova@gmail.com>
2020-07-03 17:42:52 +0200

    Authentification: Signing, Logging in and logging out

--- app.js
+++ app.js
@@ -3,6 +3,8 @@ var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
+var session = require('express-session');
+var FileStore = require('session-file-store')(session);
 
 
 var indexRouter = require('./routes/index');
@@ -33,42 +35,33 @@ app.set('view engine', 'jade');
 app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
-app.use(cookieParser('12345-67890-09876-54321'));
+//app.use(cookieParser('12345-67890-09876-54321'));
+app.use(session({
+  name: 'session_id',
+  secret: '12345-67890-09876-54321',
+  saveUninitialized: false,
+  resave: true,
+  store: new FileStore(),
+}));
+
+//authentification
+app.use('/', indexRouter);
+app.use('/users', usersRouter);
 
 function auth(req, res, next) {
-  console.log(req.signedCookies);
-  if (!req.signedCookies.user) {
-    var authHeader = req.headers.authorization;
-    if (!authHeader) {
+  console.log(req.session);
+  if (!req.session.user) {
       var err = new Error('You shall not pass without authentification!');
-      res.setHeader('WWW-Authenticate', 'Basic');
       err.status = 401;
       next(err);
-    }
-
-    var auth = new Buffer.from(authHeader.split(' ')[1], 'base64').toString().split(':'); /*splits header "Basic user:password" and keep user:password. After splits pair into user and password*/
-    var username = auth[0];
-    var password = auth[1];
-
-    if (username === 'admin' && password === 'password') {
-      res.cookie('user', 'admin', { signed: true });
-      next();
-    }
-    else {
-      var err = new Error('You shall not pass without authentification!');
-      res.setHeader('WWW-Authenticate', 'Basic');
-      err.status = 401;
-      next(err);
-    }
-  }
+  }    
   else {
-    if (req.signedCookies.user === 'admin') {
+    if (req.session.user === 'authentificated') {
       next();
     }
     else {
       var err = new Error('You shall not pass without authentification!');
-      res.setHeader('WWW-Authenticate', 'Basic');
-      err.status = 401;
+      err.status = 403;
       next(err);
     }
   }
@@ -77,8 +70,7 @@ function auth(req, res, next) {
 app.use(auth);
 app.use(express.static(path.join(__dirname, 'public')));
 
-app.use('/', indexRouter);
-app.use('/users', usersRouter);
+
 app.use('/dishes', dishRouter);
 app.use('/promotions', promoRouter);
 app.use('/leaders', leaderRouter);
